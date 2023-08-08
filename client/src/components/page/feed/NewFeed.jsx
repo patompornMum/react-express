d14c971513b40cf702d1caad6450435837713ce5
mum patomporn <ch.patomporn_st@tni.ac.th>
2023-08-08 13:58:08 +0700

    [client] New Feed Preview image

--- client/src/components/page/feed/NewFeed.jsx
+++ client/src/components/page/feed/NewFeed.jsx
@@ -1,10 +1,24 @@
-import { Button, Card, CardContent, Container, Grid, TextField, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Card, CardContent, Container, Grid, Input, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
 
 const NewFeed = () => {
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleChangeImage = (e) => {
+        console.log('change image')
+        const file = e.target.files[0];
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImagePreview(reader.result);
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
     return (
-        // <Container maxWidth={false} sx={{ padding: 2 }}>
-        // </Container>
         <Container maxWidth="md" sx={{ padding: 2 }}>
             <Card variant='outlined'>
                 <CardContent>
@@ -13,6 +27,22 @@ const NewFeed = () => {
                             <Typography variant='h4' textAlign='center'>
                                 New Feed !
                             </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <img
+                                src={imagePreview}
+                                style={{ width: '100%' }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                type="file"
+                                onChange={handleChangeImage}
+                                inputProps={{
+                                    accept: ".jpg, .png",
+                                }}
+                            />
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
