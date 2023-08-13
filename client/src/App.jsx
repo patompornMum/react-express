8119f93a4b15dc68aeebc1cc0684d7846d8cd5c0
patompornMum <ch.patomporn_st@tni.ac.th>
2023-08-13 17:16:35 +0700

    [client] setup themeProvide, CssBaseLine

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,71 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css'
-
-// Components
-import HeaderBar from "./components/HeaderBar";
+import './App.css';
 
 //Page
 import Notfound404 from "./components/page/Notfound404";
 import Login from './components/page/auth/Login';
 import Register from "./components/page/auth/Register";
-import UserManage from "./components/page/user/UserManage";
 import Feed from './components/page/feed/Feed';
 import NewFeed from "./components/page/feed/NewFeed";
+import UserManage from "./components/page/user/UserManage";
 
 //Protect Route
 import { CheckAuth } from './router/CheckAuth';
 
 //Layouts
-import MainLayout from "./layouts/MainLayout";
 import UpdateFeed from "./components/page/feed/EditFeed";
+import MainLayout from "./layouts/MainLayout";
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function App() {
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'light'
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<Notfound404 text="Page Not Found" />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="*" element={<Notfound404 text="Page Not Found" />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
 
         //Route Admin
-        <Route
-          element={<CheckAuth redirectPath='/login' role='admin'><MainLayout /></CheckAuth>}
-        >
-          <Route path="/user-manage" element={<UserManage />} />
-        </Route>
+          <Route
+            element={<CheckAuth redirectPath='/login' role='admin'><MainLayout /></CheckAuth>}
+          >
+            <Route path="/user-manage" element={<UserManage />} />
+          </Route>
 
 
         //Route User
-        <Route
-          element={<CheckAuth redirectPath='/login' role='user'><MainLayout /></CheckAuth>}
-        >
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/feed/new" element={<NewFeed />} />
-          <Route path="/feed/edit/:id" element={<UpdateFeed />} />
-        </Route>
+          <Route
+            element={<CheckAuth redirectPath='/login' role='user'><MainLayout /></CheckAuth>}
+          >
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/feed/new" element={<NewFeed />} />
+            <Route path="/feed/edit/:id" element={<UpdateFeed />} />
+          </Route>
 
         //ของจารสามิตร์
-        {/* <Route element={<ProtectedRoute><BackendLayout /></ProtectedRoute>}>
+          {/* <Route element={<ProtectedRoute><BackendLayout /></ProtectedRoute>}>
           <Route path={DASHBOARD_PATH} element={<Dashboard />} />
           <Route path={PRODUCT_PATH} element={<Product />} />
         </Route> */}
 
-      </Routes>
+        </Routes>
 
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
